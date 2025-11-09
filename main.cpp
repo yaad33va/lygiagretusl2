94c64d60ce027d6280d7861894dbda173c7f1e5e
yaad33va <denisa.valinciute@ktu.edu>
2025-11-09 18:02:14 +0200

    init commit

---
+++ main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by denva on 11/9/25.
+//
+
+#include "main.h"
---
+++ main.h
@@ -0,0 +1,13 @@
+//
+// Created by denva on 11/9/25.
+//
+
+#ifndef LYGIAGRETUSL2_MAIN_H
+#define LYGIAGRETUSL2_MAIN_H
+
+
+class main {
+};
+
+
+#endif //LYGIAGRETUSL2_MAIN_H
